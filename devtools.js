a4a1f450ea7b6a952ebcb8d4bff7276288ba5f76
Stian Didriksen <stipsan@gmail.com>
2014-12-31 14:01:29 +0100

    closes #1
    Simple prototype is working

--- devtools.js
+++ devtools.js
@@ -1,23 +1,10 @@
 // Copyright (c) 2015 Stian Didriksen. All rights reserved.
 // @TODO Will be a MIT license
 
-// The function below is executed in the context of the inspected page.
-var page_getProperties = function() {
-  var data = window.getComputedStyle($0);
-  // Make a shallow copy with a null prototype, so that sidebar does not
-  // expose prototype.
-  var props = Object.getOwnPropertyNames(data);
-  var copy = { __proto__: null };
-  for (var i = 0; i < props.length; ++i)
-    copy[props[i]] = data[props[i]];
-  return copy;
-}
-
 chrome.devtools.panels.elements.createSidebarPane(
     "Watch Expressions",
     function(sidebar) {
   function updateElementProperties() {
-    console.log("(" + page_getProperties.toString() + ")()");
     sidebar.setPage('panel.html');
     //sidebar.setExpression("(" + page_getProperties.toString() + ")()", 'title', {}, '', function(){console.log('callback!', this, arguments)});
     console.log(chrome.devtools);
--- panel.js
+++ panel.js
@@ -1,3 +1,27 @@
 console.log(this);
 
 document.body.innerHTML = '<h1>Hi!</h1>';
+
+var page_getProperties = function() {
+  var data = window.getComputedStyle(document.querySelector('a'));
+  // Make a shallow copy with a null prototype, so that sidebar does not
+  // expose prototype.
+  var props = Object.getOwnPropertyNames(data);
+  var copy = { __proto__: null };
+  for (var i = 0; i < props.length; ++i) {
+    if(props[i] == 'color') {
+      copy[props[i]] = data[props[i]];
+
+    }
+  }
+  return copy;
+}
+
+var expr = "(" + page_getProperties.toString() + ")()";
+chrome.devtools.inspectedWindow.eval(expr, function(result, isException){
+  if (isException) {
+    throw new Error('Eval failed for ' + expr, isException.value);
+  } else {
+    document.body.innerHTML = JSON.stringify(result);
+  }
+});
