fe3876c5f12ba664d971bda41ced160eebf588fe
Stian Didriksen <stipsan@gmail.com>
2014-12-31 11:26:42 +0100

    Get the computed style

--- devtools.js
+++ devtools.js
@@ -1,10 +1,9 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2015 Stian Didriksen. All rights reserved.
+// @TODO Will be a MIT license
 
 // The function below is executed in the context of the inspected page.
 var page_getProperties = function() {
-  var data = window.jQuery && $0 ? jQuery.data($0) : {};
+  var data = window.getComputedStyle($0);
   // Make a shallow copy with a null prototype, so that sidebar does not
   // expose prototype.
   var props = Object.getOwnPropertyNames(data);
